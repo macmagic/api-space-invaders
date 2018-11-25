487fdc7f263d5f7bb66bae66ebc877ba06289ce7
macmagic <arroyes89@gmail.com>
2018-11-25 12:44:31 +0100

    First commit!

---
+++ src/main/java/com/juanarroyes/apispaceinvaders/ApiSpaceInvadersApplication.java
@@ -0,0 +1,12 @@
+package com.juanarroyes.apispaceinvaders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpaceInvadersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpaceInvadersApplication.class, args);
+	}
+}
---
+++ src/test/java/com/juanarroyes/apispaceinvaders/ApiSpaceInvadersApplicationTests.java
@@ -0,0 +1,16 @@
+package com.juanarroyes.apispaceinvaders;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiSpaceInvadersApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
